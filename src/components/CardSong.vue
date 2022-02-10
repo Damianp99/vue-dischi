376a3e6a7ca2a463efcf82f550d0cadc02648004
Damianp99 <damianperez200199@gmail.com>
2022-02-10 18:28:04 +0100

    added v-for with all cards

--- src/components/CardSong.vue
+++ src/components/CardSong.vue
@@ -1,23 +1,11 @@
 <template>
   <div>
-    <div
-      v-for="(card, index) in cards"
-      :key="index"
-      class="
-        col-2
-        offset-1
-        bg-card-song
-        d-flex
-        flex-column
-        align-items-center
-        text-center
-      "
-    >
+    <div class="bg-card-song">
       <div class="card-song">
         <img :src="poster" alt="song" />
       </div>
       <div class="card-description">
-        <h4 class="text-white">{{ title }}</h4>
+        <span class="text-white">{{ title }}</span>
         <div class="text-muted h5">
           <span>{{ author }}</span>
         </div>
@@ -30,40 +18,21 @@
 </template>
 
 <script>
-import axios from "axios";
 export default {
   name: "Card",
-  data() {
-    return {
-      cards: [],
-    };
-  },
-  methods: {
-    // getSongs() {
-    //   axios
-    //     .get("https://flynn.boolean.careers/exercises/api/array/music")
-    //     .then((res) => {
-    //       this.cards = res.data.response;
-    //     });
-    // },
-  },
-  mounted() {
-    axios
-      .get("https://flynn.boolean.careers/exercises/api/array/music")
-      .then((res) => {
-        this.cards = res.data.response;
-      });
-  },
+  props: ["poster", "title", "author", "year"],
 };
 </script>
 
 <style lang="scss" scoped>
 @import "~bootstrap/scss/bootstrap.scss";
+
 .bg-card-song {
   background-color: #2e3a46;
 }
 .card-description {
-  padding: 20px;
+  height: 150px;
+  width: 200;
 }
 
 .card-song {
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,23 +2,57 @@
   <main>
     <div class="container">
       <div class="row py-5">
-        <CardSong />
+        <dinpv
+          v-for="(card, index) in cards"
+          :key="index"
+          class="col-2 my-1 justify-content-center text-center d-flex px-5"
+        >
+          <CardSong
+            :poster="card.poster"
+            :title="card.title"
+            :author="card.author"
+            :year="card.year"
+          />
+        </div>
       </div>
     </div>
   </main>
 </template>
 
 <script>
+import axios from "axios";
 import CardSong from "./CardSong.vue";
+
 export default {
   name: "Main",
   components: {
     CardSong,
   },
+  data() {
+    return {
+      cards: [],
+    };
+  },
+  methods: {
+    // getSongs() {
+    //   axios
+    //     .get("https://flynn.boolean.careers/exercises/api/array/music")
+    //     .then((res) => {
+    //       this.cards = res.data.response;
+    //     });
+    // },
+  },
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((res) => {
+        this.cards = res.data.response;
+      });
+  },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" >
 @import "~bootstrap/scss/bootstrap.scss";
 main {
   height: calc(100vh - 80px);
